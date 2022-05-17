e1f9b02a78f6f1f3182f1831c282ba007f91c607
ChrisLiang7 <zliang15@calpoly.edu>
2022-05-17 12:38:34 -0700

    add structures and create the navbar

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import SubHeading from '../../components/SubHeading/SubHeading';
+import { images } from '../../constants';
+import './Header.css';
+
+
+const Header = () => (
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper__info">
+      <SubHeading  />
+    </div>
+
+    <div className="app__wrapper__img">
+      
+    </div>
+
+  </div>
+);
+
+export default Header;
