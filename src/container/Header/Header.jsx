62ede57332c8ff8de0c2ec34a8750cd7e4bb092b
ChrisLiang7 <zliang15@calpoly.edu>
2022-05-17 19:28:02 -0700

    update header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -6,12 +6,15 @@ import './Header.css';
 
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
-    <div className="app__wrapper__info">
-      <SubHeading  />
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavor" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0'}}>Sit Tellus Lobortis Sed Senectus Vivamus Molestie. Condimentum Volutpat Morbi Facilisis Quam Scelerisque Sapien. Et, Penatibus Aliquam Amet Tellus</p>
+      <button type="button" class="custom__button">Explore Menu</button>
     </div>
 
-    <div className="app__wrapper__img">
-      
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header_img" />
     </div>
 
   </div>
